f5d53349ee4f181f7424963b3b59a564c2cffd8f
sj507 <75410678+sj507@users.noreply.github.com>
2021-11-08 15:10:28 +0000

    Update Pebble.java

--- Pebble.java
+++ Pebble.java
@@ -1,3 +1,8 @@
+/**
+ * Pebble class, for creating all the pebbles that we would need and storing any methods that an individual pebble might need.
+ * 
+ * @authors Sam Judges, Matt Morris
+ */
 
 public class Pebble
 {
